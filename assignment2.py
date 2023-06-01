e2ae1ac29171ce66688b6b3da32b488e7e7fd25b
dibyendu-devops <129838157+dibyendu-devops@users.noreply.github.com>
2023-06-01 10:22:43 +0530

    Add files via upload
    second assignment of the session

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+list = [ 2,3,4,55,56,78,75,69,66,101,100 ]
+even_count, odd_count = 0, 0
+# enhanced for loop
+for num in list:
+   #even numbers
+   if num % 2 == 0:
+      even_count += 1
+   #odd numbers
+   else:
+      odd_count += 1
+print("Even numbers available in the list: ", even_count)
+print("Odd numbers available in the list: ", odd_count)
