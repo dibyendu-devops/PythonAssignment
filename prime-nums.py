a110a85042d1ea9f2dcd9eedf1911e5c27fb4063
dibyendu-devops <129838157+dibyendu-devops@users.noreply.github.com>
2024-07-24 20:02:21 +0200

    Create prime-nums.py

---
+++ prime-nums.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number "))
+for i in range (2,num):
+  if num % i == 0:
+    print("The entered number is Not prime ")
+    break
+else:
+    print("The entered number is Prime")
+
+  
+      
