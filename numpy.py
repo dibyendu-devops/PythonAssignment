5bed1db5627b57a666ed124ecd96f3f980afaccd
dibyendu-devops <129838157+dibyendu-devops@users.noreply.github.com>
2024-07-25 19:10:30 +0200

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,28 @@
+from numpy import *
+arr = array([1,2,4,2,3,4])
+   
+print(arr)
+
+output will shows as below::
+
+[6 7 9 7 8 9]
+
+
+from numpy import *
+arr = array([1,2,4,2,3,4])
+print(sum(arr))
+
+16
+
+from numpy import *
+arr = array([0, 30, 45, 60])
+print(sin(arr))
+
+from numpy import *
+arr = array([0, 30, 45, 60])
+print(max(arr))
+
+from numpy import *
+arr = array([0, 30, 45, 60, 35, 50])
+print(sort(arr))
+
