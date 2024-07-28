293c7a47f7de6094a7121aa3ec23448c03f74aa7
dibyendu-devops <129838157+dibyendu-devops@users.noreply.github.com>
2024-07-28 19:09:19 +0200

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -26,3 +26,42 @@ from numpy import *
 arr = array([0, 30, 45, 60, 35, 50])
 print(sort(arr))
 
+how to copy array:
+
+from numpy import *
+#arr = array([0, 30, 45, 60, 35, 50])
+#print(sort(arr))
+
+arr1 = array([2, 5, 6, 8])
+arr2=arr1.view()
+print(arr1,arr2)
+print(id(arr1))
+print(id(arr2))
+with the view the id will be different and without view it will be same
+
+## if you wish to copy the arrays but later you want to change the value of one of the array.
+## Shallow copy - it will change for second array as well.
+## Deep Copy - it will not change the value for second array
+
+
+from numpy import *
+#arr = array([0, 30, 45, 60, 35, 50])
+#print(sort(arr))
+
+arr1 = array([2, 5, 6, 8])
+arr2=arr1.copy()
+arr1[2]=7
+print(arr1,arr2)
+
+print(id(arr1))
+print(id(arr2))
+
+## in Output the value will be changed to 7 
+
+
+
+########################write a code to add 2 arrays using for loop###############################
+
+
+write a code to find the maxium value from an array without using in-build function
+
