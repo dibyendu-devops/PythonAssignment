2c06aa422471fd15da0a3c451a3dc2e9a8eccdc6
dibyendu-devops <129838157+dibyendu-devops@users.noreply.github.com>
2024-07-30 19:40:18 +0200

    Create function.py

---
+++ function.py
@@ -0,0 +1,20 @@
+In python, we can define a function using def function_name put () and : like for an example: So here great is the func
+
+def great():
+    print("Hello Dibyendu")
+    print("Good Morning")
+def add(x, y):
+   c = x + y
+   print(c)
+great()
+add(5, 8)
+
+
+
+def add_sub(x,y):
+    c = x+y
+    d=x-y
+    return c,d
+result1, result2 = add_sub(8,5)
+print(result1,result2)
+
